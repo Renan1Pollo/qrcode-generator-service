0b907c9b7d414f9683ecd212bce952d6575d9c56
Renan Pollo Benelli <renansk18@outlook.com>
2024-08-30 20:33:39 -0300

    first commit

---
+++ src/main/java/com/pollo/qrcodeservice/QrcodeServicetApplication.java
@@ -0,0 +1,13 @@
+package com.pollo.qrcodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeServicetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodeServicetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pollo/qrcodeservice/QrcodeServicetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollo.qrcodeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeServicetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
